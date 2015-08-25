95ea9fab72d47e61b444b9b9764ce8932afaac49
Judge Madan <judge.madan@gmail.com>
2015-08-25 18:27:45 +0700

    Added Extension Popup, Finished GPA Calculation Algorithm

--- src/gpa.js
+++ src/gpa.js
@@ -1,15 +1,91 @@
-var data = document.documentElement.outerHTML;
+var gradeData = document.getElementsByTagName("tbody")[1].innerHTML;
+var classData = document.getElementsByTagName("tbody")[1].innerHTML;
+var letterGrades = [];
+var className = [];
+var GP = 0;
+var GPA;
+var change = document.getElementsByTagName("tbody")[2];
 
-//get array of regex matches
-data = data.match(/scores\.html.*<\/a>/g);
+/*
+ * CLASS
+ */
+classData = classData.match(/"left">.*<br>/g);
 
-//formatting regex matches to grab grades
-for (i = 0; i < data.length; i++) {
-	data[i] = data[i].slice(0,-4);
-	data[i]=data[i].substring(data[i].indexOf(">")+1,data[i].length);
+for ( i = 0; i < classData.length; i++) {
+	classData[i] = classData[i].slice(7);
 }
 
-console.log(data); 
+/*
+* GRADES
+*/
 
-//grade check regex
-// /[ABCDF].?/g
+//get array of regex matches
+gradeData = gradeData.match(/scores\.html.*<\/a>/g);
+
+//formatting regex matches to grab grades
+for ( i = 0; i < gradeData.length; i++) {
+	//isolate grades
+	gradeData[i] = gradeData[i].slice(0, -4);
+	gradeData[i] = gradeData[i].substring(gradeData[i].indexOf(">") + 1, gradeData[i].length);
+
+	//recheck grades and isolate letters
+	gradeData[i] = gradeData[i].match(/[ABCDF][+-]?/g);
+}
+
+/*
+ * PROCESSING
+ */
+for ( i = 0; i < gradeData.length; i++) {
+	//if grades exist then push class and grades into array.
+	if (gradeData[i]) {
+		letterGrades.push(gradeData[i][0]);
+		className.push(classData[i])
+	}
+}
+
+//IB WEIGHT
+for ( i = 0; i < className.length; i++) {
+	if ((className[i].substring(0, 2) == "IB" || className[i].substring(0, 2) == "AP") && (className[i] != "IB Math Studies SL1" && className[i] != "IB Math Studies SL2")) {
+		GP = GP + 0.5;
+	}
+}
+
+//GPA CALC
+function calcGP(grade) {
+	if (grade == "A+") {
+		GP = GP + 4.3;
+	} else if (grade == "A") {
+		GP = GP + 4;
+	} else if (grade == "A-") {
+		GP = GP + 3.7;
+	} else if (grade == "B+") {
+		GP = GP + 3.3;
+	} else if (grade == "B") {
+		GP = GP + 3;
+	} else if (grade == "B-") {
+		GP = GP + 2.7;
+	} else if (grade == "C+") {
+		GP = GP + 2.3;
+	} else if (grade == "C") {
+		GP = GP + 2;
+	} else if (grade == "C-") {
+		GP = GP + 1.7;
+	} else if (grade == "D+") {
+		GP = GP + 1.3;
+	} else if (grade == "D") {
+		GP = GP + 1;
+	} else if (grade == "D-") {
+		GP = GP + 0.7;
+	} else {
+	}
+}
+
+for ( i = 0; i < letterGrades.length; i++) {
+	calcGP(letterGrades[i]);
+}
+
+GPA = GP / letterGrades.length;
+
+change.innerHTML = "<p style='font-family:HelveticaNeue-Light, Helvetica Neue, Helvetica;font-size: 20px;text-align:center;'>(S1) GPA: " + parseFloat(GPA).toFixed(3); +"</p>";
+
+
